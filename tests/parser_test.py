9c251e802167c508b03f462229dd55da559f5b3e
Andres Gordon <gordon_andres@yahoo.es>
2022-11-18 15:29:11 -0500

    🎨 [ADD] Error test

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -64,3 +64,12 @@ class ParserTest(TestCase):
         expected_names: List[str] = ['x', 'y', 'foo']
 
         self.assertEqual(names, expected_names)
+
+    def test_parse_errors(self) -> None:
+        source: str = 'variable x 5;'
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        self.assertEqual(len(parser.errors), 1)

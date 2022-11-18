e7ecdf672ef7d8e38db41fe0704344987efb5568
Andres Gordon <gordon_andres@yahoo.es>
2022-11-17 20:56:01 -0500

    🎨 [ADD] Repl to main

---
+++ main.py
@@ -0,0 +1,11 @@
+from lpp.repl import start_repl
+
+
+def main() -> None:
+    print('Listo para la diversion?')
+    print('Let\'s go!')
+    start_repl()
+
+
+if __name__ == '__main__':
+    main()

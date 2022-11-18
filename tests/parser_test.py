9367da8dc5838dc479a47de338475acbd7bb8ce0
Andres Gordon <gordon_andres@yahoo.es>
2022-11-18 15:10:39 -0500

    🎨 [ADD] Let statements names test

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,5 +1,10 @@
 from unittest import TestCase
 
+from typing import (
+    cast,
+    List,
+)
+
 from lpp.ast import (
     Program,
     LetStatement,
@@ -36,3 +41,26 @@ class ParserTest(TestCase):
         for statement in program.statements:
             self.assertEqual(statement.token_literal(), 'variable')
             self.assertIsInstance(statement, LetStatement)
+
+    def test_names_in_let_statement(self) -> None:
+        source: str = '''
+            variable x = 5;
+            variable y = 10;
+            variable foo = 20;
+        '''
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        self.assertEqual(len(program.statements), 3)
+
+        names: List[str] = []
+        for statement in program.statements:
+            statement = cast(LetStatement, statement)
+            assert statement.name is not None
+            names.append(statement.name.value)
+
+        expected_names: List[str] = ['x', 'y', 'foo']
+
+        self.assertEqual(names, expected_names)
